c948e00e7ca03f7f571f061e277043ac5a2574af
wangjitao <767884538@qq.com>
2019-04-02 13:41:06 +0800

    vue

---
+++ base/constants.js
@@ -0,0 +1,26 @@
+// 全局变量
+
+
+const   asd=new Object();
+asd.path="";
+
+
+// var定义的变量可以修改，如果不初始化会输出undefined，不会报错。
+//const定义的变量不可以修改，而且必须初始化。
+//let是块级作用域，函数内部使用let定义后，对函数外部无影响。
+
+const order_json =
+    {
+        "name": "11111",
+        "name2": "22222",
+        "name3": "33333"
+    };
+const waldenWebHost = "139.199.26.87";
+
+const vue_test = "https://cdn.jsdelivr.net/npm/vue";
+
+const vue_prod = "https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js";
+
+const vue_path = vue_test;
+
+
